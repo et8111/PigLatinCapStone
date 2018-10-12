c45aabf45c34158b2d6fa17997fa33d99f123659
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-10-12 10:19:59 -0400

    Added number and special character finder

--- PigLatinCapStone/Program.cs
+++ PigLatinCapStone/Program.cs
@@ -10,7 +10,9 @@ namespace PigLatinCapStone
     class Program
     {
         public static string puncuationChecker(string str)
-        {
+        {//if theres puncuation send it to VOwelSeparator, if not send an empty string
+            if (Regex.Match(str, "[0-9@#$%^&*()<>]").Success)
+                return str;
             string temp = "";
             if (Regex.Match(str, "[.,!?]").Success)
             {
@@ -20,15 +22,15 @@ namespace PigLatinCapStone
             return VowelSeparator(str, temp);
         }
 
-        public static string VowelSeparator(string str, string p)
-        {
+        private static string VowelSeparator(string str, string p)
+        {//j indexes first vowel. add 2 substrings plus pigLatin ending and punctuation if one exist sent to capitalLetter.
             int j = Regex.Match(str, "[aeiouAEIOU]").Index;
             string temp = (j == 0)?"way":"ay";
             return CapitalLetter(str.Substring(j, str.Length-j) +str.Substring(0,j)+ temp+ p);
         }
 
-        public static string CapitalLetter(string str)
-        {
+        private static string CapitalLetter(string str)
+        {//This function assumes if there is a capital letter its supposed to be the first one.
             if (Regex.IsMatch(str, @"^.*[A-Z].*$"))
                 str = char.ToUpper(str[0]) + str.Substring(1, str.Length - 1).ToLower();
             return str;
@@ -37,14 +39,15 @@ namespace PigLatinCapStone
         static void Main(string[] args)
         {
             string choice = "";
-            string[] word;
+            string[] word, final;
             while (true)
             {
                 Console.WriteLine("ENTER A STRING: ");
                 word = Console.ReadLine().Split(' ');
+                final = new string[word.Length];
                 for (int i = 0; i < word.Length; i++) //puncuationChecker->vowelSeperator->CapitalLetter: back here
-                    word[i] = puncuationChecker(word[i]);
-                Console.WriteLine(string.Join(" ", word));
+                    final[i] = puncuationChecker(word[i]);
+                Console.WriteLine(string.Join(" ", final));
             Ask:
                 Console.Write("\nAgain (y/n)?: ");
                 choice = Console.ReadLine();
